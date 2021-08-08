52cafca25429065b0a4ffab228ab5b1273e6f3dd
jedcheng <whaleshark86@gmail.com>
2021-08-08 20:24:05 +0100

    Update setup.py

--- FOTS/rroi_align/setup.py
+++ FOTS/rroi_align/setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
-    name='rotate_roi',
+    name='rotated_roi',
     ext_modules=[
-        CUDAExtension('rotate_roi', [
+        CUDAExtension('rotated_roi', [
             'src/rroi_align_cuda.cpp',
             'src/rroi_align_kernel.cu',
         ])
